03dfdc440ed5473739adcbb610b5263cdf255572
Jjedrysz <80046957+Jjedrysz@users.noreply.github.com>
2024-06-06 21:33:43 +0200

    Add files via upload

--- kalkulator/app/calc.php
+++ kalkulator/app/calc.php
@@ -1,50 +1,56 @@
 <?php
-// KONTROLER strony kalkulatora
 require_once dirname(__FILE__).'/../config.php';
+
+// KONTROLER strony kalkulatora
 
 // W kontrolerze niczego nie wysyła się do klienta.
 // Wysłaniem odpowiedzi zajmie się odpowiedni widok.
 // Parametry do widoku przekazujemy przez zmienne.
 
-// 1. pobranie parametrów
+//ochrona kontrolera - poniższy skrypt przerwie przetwarzanie w tym punkcie gdy użytkownik jest niezalogowany
+include _ROOT_PATH.'/app/security/check.php';
 
-$x = $_REQUEST ['x'];
-$currency = $_REQUEST ['c'];
-
-// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
-
-// sprawdzenie, czy parametry zostały przekazane
-if ( ! (isset($x) && isset($currency))) {
-	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
-	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+//pobranie parametrów
+function getParams(&$x,&$currency){
+	$x = isset($_REQUEST['x']) ? $_REQUEST['x'] : null;
+	$y = isset($_REQUEST['y']) ? $_REQUEST['y'] : null;
+	$currency = isset($_REQUEST['c']) ? $_REQUEST['c'] : null;	
 }
 
-// sprawdzenie, czy potrzebne wartości zostały przekazane
-if ( $x == "") {
-	$messages [] = 'Nie podano liczby 1';
-}
-
-//nie ma sensu walidować dalej gdy brak parametrów
-if (empty( $messages )) {
-	
-	// sprawdzenie, czy $x i $y są liczbami całkowitymi
-	if (! is_numeric( $x )) {
-		$messages [] = 'Podana wartość nie jest liczbą całkowitą';
+//walidacja parametrów z przygotowaniem zmiennych dla widoku
+function validate(&$x,&$currency,&$messages){
+	// sprawdzenie, czy parametry zostały przekazane
+	if ( ! (isset($x) && isset($currency))) {
+		// sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+		// teraz zakładamy, ze nie jest to błąd. Po prostu nie wykonamy obliczeń
+		return false;
 	}
 
+	// sprawdzenie, czy potrzebne wartości zostały przekazane
+	if ( $x == "") {
+		$messages [] = 'Nie podano kwoty';
+	}
+		//nie ma sensu walidować dalej gdy brak parametrów
+	if (count ( $messages ) != 0) return false;
+		
+	if (count ( $messages ) != 0) return false;
+	else return true;
 }
 
-// 3. wykonaj zadanie jeśli wszystko w porządku
-
-if (empty ( $messages )) { // gdy brak błędów
-	
+function process(&$x,&$currency,&$messages,&$result){
+	global $role;
+		
 	//konwersja parametrów na int
 	$x = intval($x);
 	
 	//wykonanie operacji
 	switch ($currency) {
 		case 'euro' :
-			$result = $x * 4.3;
+		if ($role == 'admin'){
+				$result = $x * 4.3;
+			} else {
+				$messages [] = 'Funkcja dostępna tylko dla administratora !';
+			}
 			break;
 		case 'dolar' :
 			$result = $x * 4;
@@ -58,7 +64,21 @@ if (empty ( $messages )) { // gdy brak błędów
 	}
 }
 
-// 4. Wywołanie widoku z przekazaniem zmiennych
-// - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
-//   będą dostępne w dołączonym skrypcie
+//definicja zmiennych kontrolera
+$x = null;
+$currency = null;
+$result = null;
+$messages = array();
+
+//pobierz parametry i wykonaj zadanie jeśli wszystko w porządku
+getParams($x,$currency);
+if ( validate($x,$currency,$messages) ) { // gdy brak błędów
+	process($x,$currency,$messages,$result);
+}
+
+$page_title = 'Kalkulator Walut';
+$page_description = 'Kalkulator pozwalający na przeliczanie złotówek na różne waluty';
+$page_header = 'Kalkulator';
+$page_footer = 'Stopka';
+
 include 'calc_view.php';
--- kalkulator/app/calc_view.php
+++ kalkulator/app/calc_view.php
@@ -1,14 +1,16 @@
 <?php require_once dirname(__FILE__) .'/../config.php';?>
-<!DOCTYPE HTML>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
-<head>
-<meta charset="utf-8" />
-<title>Kalkulator</title>
-</head>
-<body>
 
-<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
-	<label for="id_x">Kwota z złotówkach: </label>
+<?php //góra strony z szablonu 
+include _ROOT_PATH.'/templates/top.php';
+?>
+
+<h2 class="content-head is-center">Prosty kalkulator walutowy</h2>
+
+<div class="pure-g">
+<div class="l-box-lrg pure-u-1 pure-u-med-2-5">
+	<form class="pure-form pure-form-stacked" action="<?php print(_APP_ROOT);?>/app/calc.php" method="post">
+		<fieldset>
+		<label for="id_x">Kwota z złotówkach: </label>
 	<input id="id_x" type="text" name="x" value="<?php print($x); ?>" /><br />
 	<label for="id_c">Waluta: </label>
 	<select name="c">
@@ -17,14 +19,19 @@
 		<option value="korona">korona</option>
 		<option value="peso">peso</option>
 	</select><br />
-	<input type="submit" value="Oblicz" />
-</form>	
+	</fieldset>	
+	<input type="submit" value="Oblicz" class="pure-button pure-button-primary" />
+	</form>
+</div>
+
+<div class="l-box-lrg pure-u-1 pure-u-med-3-5">
 
 <?php
 //wyświeltenie listy błędów, jeśli istnieją
 if (isset($messages)) {
 	if (count ( $messages ) > 0) {
-		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+	echo '<h4>Wystąpiły błędy: </h4>';
+	echo '<ol class="err">';
 		foreach ( $messages as $key => $msg ) {
 			echo '<li>'.$msg.'</li>';
 		}
@@ -33,11 +40,30 @@ if (isset($messages)) {
 }
 ?>
 
+<?php
+//wyświeltenie listy informacji, jeśli istnieją
+if (isset($infos)) {
+	if (count ( $infos ) > 0) {
+	echo '<h4>Informacje: </h4>';
+	echo '<ol class="inf">';
+		foreach ( $infos as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
 <?php if (isset($result)){ ?>
-<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
-<?php echo 'Wynik: '.$result; ?>
-</div>
+	<h4>Wynik</h4>
+	<p class="res">
+<?php print($result); ?>
+	</p>
 <?php } ?>
 
-</body>
-</html>
+</div>
+</div>
+
+<?php //dół strony z szablonu 
+include _ROOT_PATH.'/templates/bottom.php';
+?>
