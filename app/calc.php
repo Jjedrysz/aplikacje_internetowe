98ae93ae75637a5cc9819c9fa56e00cdb5637fad
Jjedrysz <80046957+Jjedrysz@users.noreply.github.com>
2024-06-30 21:30:14 +0200

    Add files via upload

--- app/calc.php
+++ app/calc.php
@@ -1,108 +1,13 @@
 <?php
+//Skrypt uruchamiający akcję wykonania obliczeń kalkulatora
+// - należy zwrócić uwagę jak znacząco jego rola uległa zmianie
+//   po wstawieniu funkcjonalności do klasy kontrolera
+
 require_once dirname(__FILE__).'/../config.php';
 
-// KONTROLER strony kalkulatora
-require_once dirname(__FILE__).'/../config.php';
-//załaduj Smarty
-require_once _ROOT_PATH.'/lib/smarty/Smarty.class.php';
+//załaduj kontroler
+require_once $conf->root_path.'/app/CalcCtrl.class.php';
 
-// W kontrolerze niczego nie wysyła się do klienta.
-// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
-// Parametry do widoku przekazujemy przez zmienne.
-
-//ochrona kontrolera - poniższy skrypt przerwie przetwarzanie w tym punkcie gdy użytkownik jest niezalogowany
-include _ROOT_PATH.'/app/security/check.php';
-
-//pobranie parametrów
-function getParams(&$form){
-	$form = isset($_REQUEST['x']) ? $_REQUEST['x'] : null;
-	$form = isset($_REQUEST['y']) ? $_REQUEST['y'] : null;
-	$form = isset($_REQUEST['c']) ? $_REQUEST['c'] : null;	
-}
-
-//walidacja parametrów z przygotowaniem zmiennych dla widoku
-function validate(&$form,&$infos,&$messages,&$hide_intro){
-	// sprawdzenie, czy parametry zostały przekazane
-	if ( ! (isset($form['x']) && isset($form['currency']))) {
-		// sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
-		// teraz zakładamy, ze nie jest to błąd. Po prostu nie wykonamy obliczeń
-		return false;
-	}
-
-	//parametry przekazane zatem
-	//nie pokazuj wstępu strony gdy tryb obliczeń (aby nie trzeba było przesuwać)
-	// - ta zmienna zostanie użyta w widoku aby nie wyświetlać całego bloku itro z tłem 
-	$hide_intro = true;
-
-	$infos [] = 'Przekazano parametry.';
-
-	// sprawdzenie, czy potrzebne wartości zostały przekazane
-	if ( $x == "") {
-		$messages [] = 'Nie podano kwoty';
-	}
-		//nie ma sensu walidować dalej gdy brak parametrów
-	if (count ( $messages ) != 0) return false;
-		
-	if (count ( $messages ) != 0) return false;
-	else return true;
-}
-
-function process(&$x,&$currency,&$messages,&$result){
-	global $role;
-		
-	//konwersja parametrów na int
-	$x = intval($x);
-	
-	//wykonanie operacji
-	switch ($currency) {
-		case 'euro' :
-		if ($role == 'admin'){
-				$result = $x * 4.3;
-			} else {
-				$messages [] = 'Funkcja dostępna tylko dla administratora !';
-			}
-			break;
-		case 'dolar' :
-			$result = $x * 4;
-			break;
-		case 'korona' :
-			$result = $x * 0.2;
-			break;
-		default :
-			$result = $x * 0.25;
-			break;
-	}
-}
-
-//inicjacja zmiennych
-$form = null;
-$infos = array();
-$messages = array();
-$result = null;
-$hide_intro = false;
-	
-getParams($form);
-if ( validate($form,$infos,$messages,$hide_intro) ){
-	process($form,$infos,$messages,$result);
-}
-
-// 4. Przygotowanie danych dla szablonu
-
-$smarty = new Smarty();
-
-$smarty->assign('app_url',_APP_URL);
-$smarty->assign('root_path',_ROOT_PATH);
-$smarty->assign('page_title','Przykład 04');
-$smarty->assign('page_description','Profesjonalne szablonowanie oparte na bibliotece Smarty');
-$smarty->assign('page_header','Szablony Smarty');
-
-$smarty->assign('hide_intro',$hide_intro);
-
-//pozostałe zmienne niekoniecznie muszą istnieć, dlatego sprawdzamy aby nie otrzymać ostrzeżenia
-$smarty->assign('form',$form);
-$smarty->assign('result',$result);
-$smarty->assign('messages',$messages);
-$smarty->assign('infos',$infos);
-
-// 5. Wywołanie szablonu
-$smarty->display(_ROOT_PATH.'/app/calc.html');
+//utwórz obiekt i użyj
+$ctrl = new CalcCtrl();
+$ctrl->process();
