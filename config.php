c6c9810efb3bf0ea0532f82f36faeb35c457947b
Jjedrysz <80046957+Jjedrysz@users.noreply.github.com>
2024-06-30 22:02:34 +0200

    Add files via upload

--- config.php
+++ config.php
@@ -1,15 +1,12 @@
 <?php
-define('_SERVER_NAME', 'localhost');
-define('_SERVER_URL', 'http://'._SERVER_NAME);
-define('_APP_ROOT', '/kalkulator');
-define('_APP_URL', _SERVER_URL._APP_ROOT);
-define("_ROOT_PATH", dirname(__FILE__));
 
-/*
-function out(&$param){
-	if (isset($param)){
-		echo $param;
-	}
-}
-*/
-?>
+
+$conf->server_name = 'localhost';
+$conf->server_url = 'http://'.$conf->server_name;
+$conf->app_root = '/kalkulator';
+$conf->action_root = $conf->app_root.'/app/ctrl.php?action=';
+
+
+$conf->action_url = $conf->server_url.$conf->action_root;
+$conf->app_url = $conf->server_url.$conf->app_root;
+$conf->root_path = dirname(__FILE__);
